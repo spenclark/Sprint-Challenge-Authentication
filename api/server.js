f9d9ea7d705a9da63e71790ed250acc64f731cb8
spenclark <spenclark@hotmail.com>
2019-12-20 08:45:29 -0800

    auth-router, model

--- api/server.js
+++ api/server.js
@@ -15,4 +15,7 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', (req, res) => {
+    res.status(418).json({api: "up"})
+})
 module.exports = server;
